4efa43250695990a1915d22842b0a9aa08f67eaa
Lee_Max <qiang.li.max@gmail.com>
2024-10-18 20:01:41 +0800

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 
 import HomeImages from '@/views/HomeImagesView.vue'
 import SelectImage from '@/views/SelectImageView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -17,7 +17,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: SelectImage
+      component: () => import('@/views/SelectImageView.vue')
     }
   ]
 })
